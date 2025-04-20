21abbdf0cb082ddbbe1808c6a4612419be3d0073
Bogdan Shmat <bogdan.shmat@wsu.edu>
2025-04-20 10:41:29 -0700

    update index

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -6,7 +6,7 @@ export PM2_HOME=/home/ec2-user/.pm2 # Specify PM2 home (use /home/ubuntu/.pm2 fo
 # Start the application using the built JS file in dist/
 # Ensure NODE_ENV is set correctly for dotenv to potentially load .env if needed,
 # although parameters are fetched directly here. PM2 can manage NODE_ENV.
-pm2 start dist/index.js --name photocomp-api --env production --update-env
+pm2 start dist/src/index.js --name photocomp-api --env production --update-env
 # Optional: Configure PM2 to restart on crashes/reboots
 # pm2 startup # Run this manually once or in user data to generate command
 # pm2 save    # Save the current process list
